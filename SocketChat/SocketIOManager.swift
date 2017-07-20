3c96177adb160fba9eacad8a2033816ed68dc25a
Sunny <sunnylee945@hotmail.com>
2017-07-20 23:34:47 +0800

    first commit

---
+++ SocketChat/SocketIOManager.swift
@@ -0,0 +1,13 @@
+//
+//  SocketIOManager.swift
+//  SocketChat
+//
+//  Created by Mac on 2017/7/20.
+//  Copyright © 2017年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class SocketIOManager: NSObject {
+
+}
